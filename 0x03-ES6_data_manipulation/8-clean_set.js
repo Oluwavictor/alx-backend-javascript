6cfc004a5efb1aae3c889c4db9c96dcdd6c0daa0
Oluwavictor <46841607+Oluwavictor@users.noreply.github.com>
2023-07-04 18:22:21 -0700

    initial commits

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,9 @@
+export default function cleanSet(set, startString) {
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
+    return '';
+  }
+  const filteredValues = Array.from(set).filter((value) => typeof value === 'string' && value.startsWith(startString));
+  const cleanedValues = filteredValues.map((value) => value.slice(startString.length));
+  return cleanedValues.join('-');
+};
+
