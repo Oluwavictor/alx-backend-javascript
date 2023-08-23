e3cf30c562ac9e63214ab5b37c72e4f8b7f0d2c1
Oluwavictor <46841607+Oluwavictor@users.noreply.github.com>
2023-08-23 02:14:46 +0100

    updated 3-read file

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,46 +1,33 @@
 const fs = require('fs');
 
-function countStudents(path) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8', (error, data) => {
-      if (error) {
-        reject(new Error('Cannot load the database'));
-        return;
-      }
-
-      const lines = data.split('\n').filter(line => line.trim() !== '');
-
-      const students = lines.slice(1).map(line => line.split(','));
-
-      const fieldCounts = {};
-      const fields = new Set();
-
-      const FIELD_INDEX = 3;
-      const NAME_INDEX = 0;
-
-      students.forEach(student => {
-        if (student.length > FIELD_INDEX) {
-          const field = student[FIELD_INDEX];
-          const name = student[NAME_INDEX];
-
-          fields.add(field);
-          fieldCounts[field] = (fieldCounts[field] || 0) + 1;
+async function countStudents(path) {
+  if (fs.existsSync(path)) {
+    return new Promise((resolve) => {
+      fs.readFile(path, 'utf8', (err, data) => {
+        if (err) {
+          throw Error('Cannot load the database');
         }
+        const result = [];
+        const students = data.split('\n');
+        students.forEach((studentData) => {
+          result.push(studentData.split(','));
+        });
+        
+        result.shift();
+        const newData = [];
+        result.forEach((studentData) => newData.push([studentData[0], studentData[3]]));
+        const fields = new Set();
+        newData.forEach((item) => fields.add(item[1]));
+        const final = {};
+        fields.forEach((fieldData) => { (final[fieldData] = 0); });
+        newData.forEach((studentData) => { (final[studentData[1]] += 1); });
+        console.log(`Number of students: ${result.filter((check) => check.length > 3).length}`);
+        Object.keys(final).forEach((fieldData) => console.log(`Number of students in ${fieldData}: ${final[fieldData]}. List: ${newData.filter((n) => n[1] === fieldData).map((n) => n[0]).join(', ')}`));
+        resolve(result, final, newData);
       });
-
-      const output = [`Number of students: ${students.length - 1}`];
-      fields.forEach(field => {
-        const studentsInField = students
-          .filter(student => student[FIELD_INDEX] === field)
-          .map(student => student[NAME_INDEX])
-          .join(', ');
-
-        output.push(`Number of students in ${field}: ${fieldCounts[field]}. List: ${studentsInField}`);
-      });
-
-      resolve(output.join('\n'));
     });
-  });
+  }
+  throw Error('Cannot load the database');
 }
 
 module.exports = countStudents;
