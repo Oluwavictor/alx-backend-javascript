fe3b3a90a65f1197b073f4481dc8610b12230c5a
Oluwavictor <oluwavictor2009@gmail.com>
2023-08-24 20:48:17 +0100

    task_9

---
+++ 0x06-unittests_in_js/9-api/api.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const app = express();
+const PORT = 7865;
+
+app.get('/', (_, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.send('Welcome to the payment system');
+});
+
+app.get('/cart/:id(\\d+)', (req, res) => {
+  const id = req.params.id;
+  console.log(`${id}`)
+  res.send(`Payment methods for cart ${id}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`API available on localhost port ${PORT}`);
+});
+
+module.exports = [app, port];
