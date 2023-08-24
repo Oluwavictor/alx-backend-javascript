1ba17f96f6d9f05c61c7f367fecaf3227038fea9
Oluwavictor <oluwavictor2009@gmail.com>
2023-08-24 21:19:17 +0100

    new commit

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -7,7 +7,7 @@ describe('make API integration test', () => {
   it('GET / returns correct response', (done) => {
     request.get(`${API_URL}/`, (_err, res, body) => {
       expect(res.statusCode).to.be.equal(200);
-      expect(body).to.be.equal('Welcome to the payment system');
+      expect(body).to.equal('Welcome to the payment system');
       done();
     });
   });
@@ -15,21 +15,21 @@ describe('make API integration test', () => {
   it('GET /cart/:id returns correct response for valid :id', (done) => {
     request.get(`${API_URL}/cart/47`, (_err, res, body) => {
       expect(res.statusCode).to.be.equal(200);
-      expect(body).to.be.equal('Payment methods for cart 47');
+      expect(body).to.equal('Payment methods for cart 47');
       done();
     });
   });
 
   it('GET /cart/:id returns 404 response for -ve number values in :id', (done) => {
     request.get(`${API_URL}/cart/-47`, (_err, res, _body) => {
-      expect(res.statusCode).to.be.equal(404);
+      expect(res.statusCode).to.equal(404);
       done();
     });
   });
 
   it('GET /cart/:id returns 404 response for non-numerical data in :id', (done) => {
     request.get(`${API_URL}/cart/d200-44a5-9de6`, (_err, res, _body) => {
-      expect(res.statusCode).to.be.equal(404);
+      expect(res.statusCode).to.equal(404);
       done();
     });
   });
